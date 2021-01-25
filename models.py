d69c2a51f2a24f207ab280f75d97c18ccdaa89a3
root <root@LAPTOP-A2EG90UI.localdomain>
2021-01-25 22:11:23 +0800

    update

--- models.py
+++ models.py
@@ -60,13 +60,14 @@ def NMT(src_vocab_size, tgt_vocab_size, input_dims, is_train = True, infer_mode 
     sampler = tfa.seq2seq.TrainingSampler();
     decoder = tfa.seq2seq.BasicDecoder(encoder, sampler, output_layer);
   else:
+    start_tokens = tf.keras.layers.Lambda(lambda x, s: tf.ones((tf.shape(x)[0],), dtype = tf.int32) * s, arguments = {'s': infer_params['start_token']})(inputs);
     if infer_mode == 'beam_search':
-      decoder = tfa.seq2seq.BeamSearchDecoder(encoder, infer_params['beam_width'], embedding_fn = embedding_layer, length_penalty_weight = infer_params['length_penalty_weight'], coverage_penalty_weight = infer_params['coverage_penalty_weight']);
+      decoder = tfa.seq2seq.BeamSearchDecoder(encoder, beam_width = infer_params['beam_width'], start_tokens = start_tokens, end_tokens = infer_params['end_token'], length_penalty_weight = infer_params['length_penalty_weight'], coverage_penalty_weight = infer_params['coverage_penalty_weight']);
     else:
       if infer_mode == 'sample':
-        sampler = tfa.seq2seq.SampleEmbeddingSampler(embedding_fn = embedding_layer, softmax_temperature = infer_params['softmax_temperature']);
+        sampler = tfa.seq2seq.SampleEmbeddingSampler(start_tokens = start_tokens, end_tokens = infer_params['end_token'], softmax_temperature = infer_params['softmax_temperature']);
       elif infer_mode == 'greedy':
-        sampler = tfa.seq2seq.GreedyEmbeddingSampler(embedding_fn = embedding_layer);
+        sampler = tfa.seq2seq.GreedyEmbeddingSampler(start_tokens = start_tokens, end_tokens = infer_params['end_token']);
       else:
         raise 'unknown infer mode!';
       decoder = tfa.seq2seq.BasicDecoder(encoder, sampler, output_layer);
@@ -75,7 +76,7 @@ def NMT(src_vocab_size, tgt_vocab_size, input_dims, is_train = True, infer_mode 
   input_lengths = tf.keras.layers.Lambda(lambda x: tf.ones((tf.shape(x)[0],), dtype = tf.int64) * tf.shape(x)[1])(inputs); # input_lengths.shape = (batch)
   input_tensors = embedding_layer(inputs);
   initial_state = decoder_cell.get_initial_state(input_tensors); # initial_state = (last_output, state)
-  output, state, lengths = decoder(input_tensors if is_train == True else inputs, sequence_length = input_lengths, initial_state = initial_state);
+  output, state, lengths = decoder(input_tensors, sequence_length = input_lengths, initial_state = initial_state);
   return tf.keras.Model(inputs = inputs, outputs = output.predicted_ids if infer_mode == 'beam_search' else output.sample_id);
 
 if __name__ == "__main__":
